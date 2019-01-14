f1988b052d979c14fe6d796f3c7bcaf9716c4bd1
samjoshuva <sam613263@gmail.com>
2019-01-14 09:41:10 +0530

    ge and post request for blockchain

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-const BlockChain = require('./blockchain/blockchain');
+const express = require('express');
+const block = require('./routes/block')
+
+const app = express();
+
+app.use(express.json())
+
+app.use('/block', block)
+
+
+app.listen(3000, () => {
+   console.log(`listening to port http://localhost:3000`)
+})
